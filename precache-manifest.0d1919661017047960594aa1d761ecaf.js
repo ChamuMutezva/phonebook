bcbb84632734bab034c35425a832b3e7776a5e2e
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-06-05 06:35:51 +0200

    Updates

---
+++ precache-manifest.0d1919661017047960594aa1d761ecaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "838155f916bafae00806f619dae5cae7",
+    "url": "/ChamuMutezva/phonebook/index.html"
+  },
+  {
+    "revision": "b7698a20b8171cc366dd",
+    "url": "/ChamuMutezva/phonebook/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3a58d3988b19acca7ab3",
+    "url": "/ChamuMutezva/phonebook/static/js/2.8e6fed29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ChamuMutezva/phonebook/static/js/2.8e6fed29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7698a20b8171cc366dd",
+    "url": "/ChamuMutezva/phonebook/static/js/main.4720e932.chunk.js"
+  },
+  {
+    "revision": "90b4d3d0939773c3320a",
+    "url": "/ChamuMutezva/phonebook/static/js/runtime-main.725b7c85.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ChamuMutezva/phonebook/static/media/logo.5d5d9eef.svg"
+  }
+]);
